aaad171c11b2bbc110f6533b5cfed4faf350d511
NechepurenkoA <nechepurenko.work@gmail.com>
2023-12-13 22:09:41 +0300

    added error handler to the application

--- src/bot/run_bot.py
+++ src/bot/run_bot.py
@@ -1,12 +1,15 @@
 from telegram import Update
+from telegram.ext import ContextTypes, CommandHandler
 
 from bot import create_bot_app
 from utils.logger import configure_logging
+from error_handler.callback_funcs import error_handler
 
 
 def main():
     configure_logging()
     application = create_bot_app()
+    application.add_error_handler(error_handler)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
