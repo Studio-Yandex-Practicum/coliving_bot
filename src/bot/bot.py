06327a2a0ac4f11b68de8ab42890ec4543c673f0
NechepurenkoA <nechepurenko.work@gmail.com>
2023-12-13 10:49:05 +0300

    fixed logging configuration place

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,8 +1,5 @@
 from telegram.ext import Application, ApplicationBuilder
 from utils.configs import TOKEN
-from utils.logger import configure_logging
-
-configure_logging()
 
 
 def create_bot_app() -> Application:
