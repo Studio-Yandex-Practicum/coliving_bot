96763a3355683c309acef8eb26b8ffb12ddebb03
NechepurenkoA <nechepurenko.work@gmail.com>
2023-12-22 16:29:41 +0300

    added new logging message template

--- src/bot/error_handler/templates.py
+++ src/bot/error_handler/templates.py
@@ -1,8 +1,14 @@
-# Пробный вариант сообщения.
+# Шаблон сообщения для пользователя Telegram.
 ERROR_MESSAGE_TEMPLATE = """
 Запрос был совершен с ошибкой, возможно, были указаны неправильные данные.
 
 Возможно проблема на стороне сервера, обязательно свяжитесь с поддержкой!
 
 <b>Ошибка: {error}</b>
+"""
+# Шаблон для логов
+LOGGING_MESSAGE_TEMPLATE = """
+"Exception while handling an update:"
+f"\ncontext.user_data={user_data}"
+f"\ncontext.chat_data={chat_data}"
 """
