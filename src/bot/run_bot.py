e7137bf014f86aacbb8293f74dcae78fcaabdb7d
NechepurenkoA <nechepurenko.work@gmail.com>
2023-12-22 16:28:58 +0300

    changed adding error_handler location

--- src/bot/run_bot.py
+++ src/bot/run_bot.py
@@ -1,14 +1,12 @@
 from telegram import Update
 
 from bot import create_bot_app
-from error_handler.callback_funcs import error_handler
 from utils.logger import configure_logging
 
 
 def main():
     configure_logging()
     application = create_bot_app()
-    application.add_error_handler(error_handler)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
