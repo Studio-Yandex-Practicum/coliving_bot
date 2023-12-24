58daa8c6ed7d88d7541a05f6beb3cbe317f87228
Artyom <144885477+NechepurenkoA@users.noreply.github.com>
2023-12-24 21:57:58 +0300

    feature/error_handler (#18)
    * added error handler to the application
    
    * created error handler
    
    * added the message template
    
    * pre-commit changes
    
    * added "add_error_handler" function
    
    * changed error_text message, added isinstance check
    
    * changed logger to constant
    
    * changed adding error_handler location
    
    * added new logging message template
    
    * new error text message
    
    * pre-commit changes
    
    * pre-commit changes
    
    * added logger name
    
    * added logger const
    
    * changed logger

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,8 +1,10 @@
 from telegram.ext import Application, ApplicationBuilder
 
+from error_handler.callback_funcs import error_handler
 from utils.configs import TOKEN
 
 
 def create_bot_app() -> Application:
     application: Application = ApplicationBuilder().token(TOKEN).build()
+    application.add_error_handler(error_handler)
     return application
