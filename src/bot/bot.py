287e23ed5330052a2d3642220d4f31776601a08c
qwertttyyy <114338111+qwertttyyy@users.noreply.github.com>
2023-12-25 18:27:06 +0300

    added "/start" command (#19)
    * added "/start" command

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,10 +1,12 @@
-from telegram.ext import Application, ApplicationBuilder
+from telegram.ext import Application, ApplicationBuilder, CommandHandler
 
+from conversations.start.callback_funcs import start
 from error_handler.callback_funcs import error_handler
 from utils.configs import TOKEN
 
 
 def create_bot_app() -> Application:
     application: Application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
     application.add_error_handler(error_handler)
     return application
