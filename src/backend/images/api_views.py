265a680e1ccca1cc626d1b67525164ae6f4edc15
Boris Belyakov <bb-87@yandex.ru>
2023-12-27 20:26:10 +0500

    edit api_views

--- src/backend/images/api_views.py
+++ src/backend/images/api_views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework import generics
-from rest_framework.permissions import SAFE_METHODS
+from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
+from rest_framework.permissions import SAFE_METHODS
 
-from profiles.models import Coliving, UserFromTelegram
+from profiles.models import Coliving, Profile, UserFromTelegram
 
-from .models import ColivingImage, ProfileImage
 from .serializers import (
     ColivingImageCreateSerializer,
     ColivingImageReadSerializer,
@@ -15,26 +13,20 @@ from .serializers import (
 )
 
 
-class BaseImage(generics.ListCreateAPIView):
-    """
-    Базовый вью-класс объектов 'ProfileImage', 'ColivingImage'.
-    """
-
-    def _get_object(self, model, **kwargs):
-        """Возвращает объекты 'Profile' или 'Coliving'."""
-        return get_object_or_404(model, **kwargs)
-
-
-class ProfileImageView(BaseImage):
+class ProfileImageView(generics.ListCreateAPIView):
     """
     Вью-класс для отображения и сохранения объектов 'ProfileImage'.
     """
 
     def get_queryset(self):
-        return ProfileImage.objects.filter(
-            profile__user__telegram_id=self._get_object(
-                UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
-            ).telegram_id
+        telegram_user = get_object_or_404(
+            UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
+        )
+        profile = Profile.objects.filter(user=telegram_user)
+        if profile.exists():
+            return profile.first().images.all()
+        raise NotFound(
+            detail="Профиль не найден", code=status.HTTP_404_NOT_FOUND
         )
 
     def get_serializer_class(self):
@@ -57,10 +49,14 @@ class ColivingImageView(generics.ListCreateAPIView):
         telegram_user = get_object_or_404(
             UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
         )
-        colivings = telegram_user.colivings.filter(id=self.kwargs.get("coliving_id"))
+        colivings = telegram_user.colivings.filter(
+            id=self.kwargs.get("coliving_id")
+        )
         if colivings.exists():
             return colivings.first().images.all()
-        raise NotFound(detail="Коливинг не найден", code=status.HTTP_404_NOT_FOUND)
+        raise NotFound(
+            detail="Коливинг не найден", code=status.HTTP_404_NOT_FOUND
+        )
 
     def get_serializer_class(self):
         return (
