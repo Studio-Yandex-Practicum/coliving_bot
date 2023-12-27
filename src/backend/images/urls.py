701f41001328a02893b5d5e79813f6b38686a291
Boris Belyakov <bb-87@yandex.ru>
2023-12-27 10:38:49 +0500

    edit urls

--- src/backend/images/urls.py
+++ src/backend/images/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from .api_views import ColivingImageView, ProfileImageView
 
 urlpatterns = [
-    path("v1/profiles/<int:id>/images/", ProfileImageView.as_view()),
-    path("v1/colivings/<int:id>/images/", ColivingImageView.as_view()),
+    path(
+        "v1/users/<int:telegram_id>/profile/images/",
+        ProfileImageView.as_view(),
+    ),
+    path(
+        "v1/users/<int:telegram_id>/coliving/<int:coliving_id>/images/",
+        ColivingImageView.as_view(),
+    ),
 ]
