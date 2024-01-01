bd46b4ddd568850c87a6cc03a5a0d16fe493db67
Boris Belyakov <bb-87@yandex.ru>
2024-01-01 21:06:14 +0500

    добавлена аннотация типов в api_views

--- src/backend/images/api_views.py
+++ src/backend/images/api_views.py
@@ -1,10 +1,14 @@
+from typing import Type
+
+from django.db.models import QuerySet
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import SAFE_METHODS
 
-from profiles.models import Profile, UserFromTelegram
+from profiles.models import Coliving, Profile, UserFromTelegram
 
+from .models import ColivingImage, ProfileImage
 from .serializers import (
     ColivingImageCreateSerializer,
     ColivingImageReadSerializer,
@@ -18,7 +22,7 @@ class BaseImageView(generics.ListCreateAPIView):
     Базовый вью-класс объектов 'ProfileImage', 'ColivingImage'.
     """
 
-    def _get_telegram_user(self):
+    def _get_telegram_user(self) -> UserFromTelegram:
         """
         Возвращает объект 'UserFromTelegram'.
         """
@@ -26,12 +30,14 @@ class BaseImageView(generics.ListCreateAPIView):
             UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
         )
 
-    def _get_coliving(self):
+    def _get_coliving(self) -> Coliving:
         """
         Возвращает объект 'Coliving'.
         """
 
-        telegram_user_colivings = self._get_telegram_user().colivings.filter(
+        telegram_user_colivings: QuerySet[
+            Coliving
+        ] = self._get_telegram_user().colivings.filter(
             id=self.kwargs.get("coliving_id")
         )
         if not telegram_user_colivings.exists():
@@ -46,22 +52,26 @@ class ProfileImageView(BaseImageView):
     Вью-класс для отображения и сохранения объектов 'ProfileImage'.
     """
 
-    def get_queryset(self):
-        profile = Profile.objects.filter(user=self._get_telegram_user())
+    def get_queryset(self) -> list[ProfileImage]:
+        profile: QuerySet[Profile] = Profile.objects.filter(
+            user=self._get_telegram_user()
+        )
         if profile.exists():
             return profile.first().images.all()
         raise NotFound(
             detail="Профиль не найден", code=status.HTTP_404_NOT_FOUND
         )
 
-    def get_serializer_class(self):
+    def get_serializer_class(
+        self,
+    ) -> Type[ProfileImageReadSerializer | ProfileImageCreateSerializer]:
         return (
             ProfileImageReadSerializer
             if self.request.method in SAFE_METHODS
             else ProfileImageCreateSerializer
         )
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer: ProfileImageCreateSerializer) -> None:
         serializer.save(
             profile=get_object_or_404(
                 Profile, user__telegram_id=self.kwargs.get("telegram_id")
@@ -74,15 +84,19 @@ class ColivingImageView(BaseImageView):
     Вью-класс для отображения и сохранения объектов 'ColivingImage'.
     """
 
-    def get_queryset(self):
+    def get_queryset(self) -> list[ColivingImage]:
         return self._get_coliving().images.all()
 
-    def get_serializer_class(self):
+    def get_serializer_class(
+        self,
+    ) -> Type[ColivingImageReadSerializer | ColivingImageCreateSerializer]:
         return (
             ColivingImageReadSerializer
             if self.request.method in SAFE_METHODS
             else ColivingImageCreateSerializer
         )
 
-    def perform_create(self, serializer):
+    def perform_create(
+        self, serializer: ColivingImageCreateSerializer
+    ) -> None:
         serializer.save(coliving=self._get_coliving())
