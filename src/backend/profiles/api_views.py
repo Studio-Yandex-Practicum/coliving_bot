7a9a289e973db034c11df88989217066074f6adb
NASTY-SMIT <nasty.smit.2004@mail.ru>
2024-01-20 20:32:19 +0700

    Исправлены замечания после созвона

--- src/backend/profiles/api_views.py
+++ src/backend/profiles/api_views.py
@@ -1,104 +1,68 @@
-from typing import Type
-
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from .models import Location, Profile, UserFromTelegram
-from .serializers import (
-    ProfileCreateSerializer,
-    ProfileReadSerializer,
-    ProfileUpdateSerializer,
-)
+from .serializers import ProfileSerializer
 
 
-class BaseProfileView(generics.ListCreateAPIView):
-    """
-    Базовый вью-класс объектов 'Profile'.
-    """
-
-    def _get_telegram_user(self) -> UserFromTelegram:
-        """
-        Возвращает объект 'UserFromTelegram'.
-        """
-        return get_object_or_404(
-            UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
-        )
-
-    def _get_location_name(self) -> Location:
-        """
-        Возвращает объект 'Location'.
-        """
-        location_name = self.request.data.get("location")
-        return Location.objects.filter(name=location_name).first()
-
-
-class ProfileView(BaseProfileView):
+class ProfileView(
+    generics.CreateAPIView, generics.RetrieveAPIView, generics.UpdateAPIView
+):
     """
     Вью-класс для отображения, сохранения и обновления объектов 'Profile'.
     """
 
-    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
     http_method_names = ["get", "post", "patch"]
-    lookup_field = "user"
-
-    def get_serializer_class(
-        self,
-    ) -> Type[
-        (ProfileCreateSerializer | ProfileReadSerializer | ProfileUpdateSerializer)
-    ]:
-        if self.request.method == "GET":
-            return ProfileReadSerializer
-        elif self.request.method == "POST":
-            return ProfileCreateSerializer
-        elif self.request.method == "PATCH":
-            return ProfileUpdateSerializer
 
     def get_object(self) -> Profile:
         queryset = self.get_queryset()
-        obj = get_object_or_404(queryset, user=self._get_telegram_user())
-        self.check_object_permissions(self.request, obj)
+        obj = get_object_or_404(
+            queryset,
+            user=get_object_or_404(
+                UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
+            ),
+        )
         return obj
 
     def get_queryset(self) -> Profile:
-        profile = Profile.objects.filter(user=self._get_telegram_user())
+        profile = Profile.objects.filter(
+            user=get_object_or_404(
+                UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
+            )
+        )
         if profile.exists():
             return profile
         raise NotFound(detail="Профиль не найден", code=status.HTTP_404_NOT_FOUND)
 
     def perform_create(self, serializer) -> None:
+        user, _is_created = UserFromTelegram.objects.get_or_create(
+            telegram_id=self.kwargs.get("telegram_id")
+        )
+        location_id, _is_created = Location.objects.get_or_create(
+            name=self.request.data.get("location")
+        )
         serializer.save(
-            user=self._get_telegram_user(),
-            location=self._get_location_name(),
+            user=user,
+            location=location_id,
             is_visible=False,
         )
 
-    def create(self, request, *args, **kwargs) -> Response:
-        serializer = self.get_serializer(
-            data=request.data, context={"request": self.request}
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
     def perform_update(self, serializer) -> None:
-        location_id = self._get_location_name()
-
-        if location_id is not None:
-            serializer.save(
-                user=self._get_telegram_user(),
-                location=location_id,
-                is_visible=self.request.data.get("is_visible", False),
-            )
-        else:
-            serializer.save(
-                user=self._get_telegram_user(),
-                is_visible=self.request.data.get("is_visible", False),
-            )
+        try:
+            location = Location.objects.get(name=self.request.data.get("location"))
+        except ObjectDoesNotExist:
+            location = self.get_object().location
+        serializer.save(
+            user=get_object_or_404(
+                UserFromTelegram, telegram_id=self.kwargs.get("telegram_id")
+            ),
+            location=location,
+            is_visible=self.request.data.get("is_visible", False),
+        )
 
     def patch(self, request, *args, **kwargs) -> Response:
         partial = kwargs.pop("partial", True)
--- src/backend/profiles/serializers.py
+++ src/backend/profiles/serializers.py
@@ -3,12 +3,15 @@ from rest_framework import serializers
 from .models import Location, Profile
 
 
-class ProfileReadSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     """
     Сериализатор объекта 'Profile' (безопасные методы).
     """
 
     user = serializers.SerializerMethodField()
+    location = serializers.SlugRelatedField(
+        queryset=Location.objects.all(), slug_field="name"
+    )
 
     class Meta:
         model = Profile
@@ -25,31 +28,3 @@ class ProfileReadSerializer(serializers.ModelSerializer):
     def get_user(self, obj):
         telegram_id = self.context["request"].parser_context["kwargs"]["telegram_id"]
         return telegram_id
-
-
-class ProfileCreateSerializer(ProfileReadSerializer):
-    """
-    Сериализатор объекта 'Profile' (создание объекта).
-    """
-
-    location = serializers.CharField()
-
-    def to_internal_value(self, data):
-        location_name = data.get("location")
-        data["location"] = location_name
-        return super().to_internal_value(data)
-
-
-class ProfileUpdateSerializer(ProfileReadSerializer):
-    """
-    Сериализатор объекта 'Profile' (обновление объекта).
-    """
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        location_id = representation.get("location")
-        if location_id is not None:
-            location = Location.objects.filter(id=location_id).first()
-            if location:
-                representation["location"] = location.name
-        return representation
