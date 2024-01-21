d3c3443fe79522ca03d1829eff8353f8dc45eac2
User Namovich <username@yandex.ru>
2024-01-21 17:41:38 +0300

    test.py

--- src/backend/profiles/serializers.py
+++ src/backend/profiles/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from .models import Location
+
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
--- src/backend/profiles/views.py
+++ src/backend/profiles/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+
 from .models import Location
 from .serializers import LocationSerializer
+
 
 class LocationList(generics.ListAPIView):
     queryset = Location.objects.all()
