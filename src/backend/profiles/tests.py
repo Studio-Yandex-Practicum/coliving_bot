ca4350ab101fa9394e17eb2050253fc5e1a44a76
User Namovich <username@yandex.ru>
2024-01-21 17:46:36 +0300

    test.py

--- src/backend/profiles/tests.py
+++ src/backend/profiles/tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework import status
-from .models import Location
 import json
+
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from .models import Location
 
 
 class LocationListTestCase(TestCase):
