f01b72c058dc859fe0dba9a452756455541b017e
Алексей Волков <alek.bishop@yandex.ru>
2024-02-13 12:30:24 +0300

    Добавлена документация

--- src/backend/coliving_bot/settings/base.py
+++ src/backend/coliving_bot/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "profiles.apps.ProfilesConfig",
     "search.apps.SearchConfig",
     "images.apps.ImagesConfig",
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -116,4 +117,12 @@ API_V1_PREFIX = "v1"
 REST_FRAMEWORK = {
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
--- src/backend/coliving_bot/urls.py
+++ src/backend/coliving_bot/urls.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("api/", include("search.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("images.urls")),
     path("api/", include("profiles.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # документация API:
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
