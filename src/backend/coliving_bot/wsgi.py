cd7ef54c43bccfb0b112ef60ba35110903f3ceba
Mikhail Bondarenko <quishimo404@ya.ru>
2024-02-16 11:04:29 +0300

    fix settings and allowed_hosts

--- src/backend/coliving_bot/wsgi.py
+++ src/backend/coliving_bot/wsgi.py
@@ -2,9 +2,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
+os.getenv(
     key="DJANGO_SETTINGS_MODULE",
-    value="coliving_bot.settings.local",
+    default="coliving_bot.settings.local"
 )
 
 application = get_wsgi_application()
