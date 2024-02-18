ccf1ac288a55b94f01277d0ed3b3cadc7b6ea889
ik <ik>
2024-02-18 17:18:52 +0300

    Add several fixes for for new EP /api/v1/profiles/ for profiles search by location, sex, age. - add ChoiceFilter for ProfilesSearchFilterSet

--- src/backend/search/filters.py
+++ src/backend/search/filters.py
@@ -1,12 +1,13 @@
-from django_filters import FilterSet, ModelChoiceFilter, RangeFilter
+from django_filters import ChoiceFilter, FilterSet, ModelChoiceFilter, RangeFilter
 
 from profiles.models import Location, Profile
+from profiles.constants import Sex
 
 
 class ProfilesSearchFilterSet(FilterSet):
     location = ModelChoiceFilter(field_name='location',
                                  queryset=Location.objects.all())
-    sex = ModelChoiceFilter(field_name='sex')
+    sex = ChoiceFilter(field_name='sex', choices=Sex.choices)
     age = RangeFilter(field_name='age')
 
     class Meta:
