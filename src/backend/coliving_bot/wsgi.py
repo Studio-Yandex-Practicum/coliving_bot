147d91ed0277fd78a7b9e0d9fe21a2a17969e5d3
Daniil Pautov <danpautoff@yandex.ru>
2024-02-20 17:50:02 +0300

    Update wsgi.py

--- src/backend/coliving_bot/wsgi.py
+++ src/backend/coliving_bot/wsgi.py
@@ -4,7 +4,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     key="DJANGO_SETTINGS_MODULE",
-    default="coliving_bot.settings.local"
+    value="coliving_bot.settings.dev"
 )
 
 application = get_wsgi_application()
