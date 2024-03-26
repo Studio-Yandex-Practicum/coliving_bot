fa11a64fa4d95e9a73b94c703bbf7f4691b3555c
unknown <C:\Users\Vlad\AppData\Roaming\The Bat!>
2024-03-27 00:02:11 +0300

    feature_delete_coliving_fix

--- src/backend/profiles/mixins.py
+++ src/backend/profiles/mixins.py
@@ -1,7 +1,20 @@
 import os
 import shutil
 
+from django.shortcuts import get_object_or_404
+
 from coliving_bot.settings.base import MEDIA_ROOT
+from profiles.models import Profile
+
+
+def delete_profile_folder(self):
+    images_dir = "profiles"
+    profile = get_object_or_404(
+        Profile, user__telegram_id=self.kwargs.get("telegram_id")
+    )
+    profile_id = str(profile.id)
+    path = os.path.join(MEDIA_ROOT, images_dir, profile_id)
+    shutil.rmtree(path)
 
 
 class DestroyWithMediaColivingRemovalMixin:
