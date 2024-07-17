5a679f7087a704adf18642dfa71d1bb0fcae281c
Daniil Pautov <danpautoff@yandex.ru>
2024-07-17 09:41:32 +0300

    Add deployment workflow. (#202)
    * Add deployment workflow.
    
    - Add deployment workflow file for production.
    - Add production settings and configuration files.
    
    * Remove sudo from production deployment commands.
    
    * Reintroduce PR merged check and update branch trigger to master in prod deploy workflow.

---
+++ src/backend/coliving_bot/settings/prod.py
@@ -0,0 +1,36 @@
+from logging.handlers import TimedRotatingFileHandler
+
+from .base import *  # noqa
+
+log_path = BASE_DIR.parent / ".data/logs"
+log_path.mkdir(parents=True, exist_ok=True)
+log_filename = log_path / "backend.log"
+
+ALLOWED_HOSTS += ["147.45.254.93"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": os.getenv("LOGGING_LEVEL", "INFO"),
+            "class": TimedRotatingFileHandler,
+            "filename": log_filename,
+            "when": os.getenv("LOGS_WHEN", "midnight"),
+            "interval": int(os.getenv("LOGS_INTERVAL", 1)),
+            "backupCount": int(os.getenv("LOGS_BACKUP_COUNT", 14)),
+            "encoding": os.getenv("LOGS_ENCODING", "utf-8"),
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+        },
+    },
+}
